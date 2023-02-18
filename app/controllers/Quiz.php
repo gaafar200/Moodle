2c09d92c8eb3378727a991a427e0ffdaf33d5bef
mahmoud <al.gaafar200@gmail.com>
2023-02-18 15:19:24 +0200

    Set Quiz Route

--- app/controllers/Quiz.php
+++ app/controllers/Quiz.php
@@ -9,11 +9,12 @@ class Quiz extends Controller
         $this->quiz = new Quizes();
     }
 
-    public function index(){
+    public function index(int $id){
         $this->data["pageName"] = "Quizes";
+        $this->data["course_id"] = $id;
         $this->view("quizzes-list",$this->data);
     }
-    public function addQuiz(int $id){
+    public function setQuiz(int $id){
         $this->data["pageName"] = "Set Quiz";
         if($_SERVER["REQUEST_METHOD"] == "POST"){
             $result = $this->quiz->setQuiz($_POST,$id);
